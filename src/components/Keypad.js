a1bde3d0fb279913643dc47d0e0f01de9cba65e7
Tri Hoang <trihoang09@gmail.com>
2023-08-21 15:57:31 -0600

    completed lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function changed(){
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={changed}/> 
+        </div>
     )
 }
 
